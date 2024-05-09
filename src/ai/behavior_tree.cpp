53de913c9f37f82d02ef532d62aa1ff7bcb237e6
HaikalMPPH <haikal.mpph65@gmail.com>
2024-05-09 09:54:34 +0700

    added bt_update with tick rate method so it ticks at a certain amount of frame.

--- src/ai/behavior_tree.cpp
+++ src/ai/behavior_tree.cpp
@@ -15,6 +15,15 @@ void BehaviorTree::bt_update() {
   _root->tick();
 }
 
+void BehaviorTree::bt_update(float tick_rate) {
+  float frame = 0;
+  if (frame >= tick_rate) {
+    _root->tick();
+    frame = 0;
+  }
+  frame++;
+}
+
 void BehaviorTree::dealloc_root() {
   delete _root;
 }
--- src/ai/behavior_tree.hpp
+++ src/ai/behavior_tree.hpp
@@ -12,6 +12,13 @@ public:
   void set_root(ABehavior* node);
   ABehavior* get_root();
   void bt_update();
+
+  /*
+     Optional: tick the bt_update() for a certain rate. Usually ties to
+     game frame rate. Example: a tick_rate of 5 means it ticks every 5
+     frame.
+  */
+  void bt_update(float tick_rate);
   void dealloc_root();
 };
 
